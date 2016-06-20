e75736279a4faaaa83c7dc073f96c76594c7eddd
Eric Guerin <eric.guerin@ucsf.edu>
2016-06-20 15:55:27 -0700

    Fix issues with it not reading entire file.  Fix image file output to be the domain name not the title tag.   Fix other minor issues.

--- screenshot.js
+++ screenshot.js
@@ -1,20 +1,32 @@
 // Run with phantomjs screenshot.js
+var fs 				= require('fs');
 
-var fs = require('fs');
-var SCREENSHOT_WIDTH = 1280;
+//some reason this doesn't work with phantomjs
+//var readline 	= require('linebyline');
+
+var domainfile 				= 'drupal7.sites.txt';
+var SCREENSHOT_WIDTH 	= 1280;
 var SCREENSHOT_HEIGHT = 900;
-var LOAD_WAIT_TIME = 5000;
-var listDrupalPages = fs.read('drupal7.sites.txt').split(/\r?\n/);
-var imageFolder = "/public/images"
-for (var i=0;i<listDrupalPages.length;i++) {
-  listDrupalPages[i]="http://"+listDrupalPages[i];
-}
-var URLS = listDrupalPages;
+var LOAD_WAIT_TIME 		= 15000;
+//raw links for filenames
+var domains 				= [];	//fs.read().split(/\r?\n/);
+var imageFolder 			= "/public/images";
+var index 						= 0;
+//formatted http links
+var URLS 							= [];
 
+
+var filedata = fs.read(domainfile); // read the file into a single string
+domains = filedata.split(/[\r\n]/); // split the string on newline and sto
+
+for (var i=0;i<domains.length;i++) {
+		 URLS[i]="http://"+domains[i];
+		 console.log(domains[i]);
+}
 
 var getPageURL = function(page){
     var getPageURL = page.url;
-    console.log("getting url:", getPageURL)
+    console.log("Getting url:", getPageURL)
     return getPageURL;
 }
 
@@ -26,55 +38,53 @@ var getPageHeight = function(page){
     var documentHeight = page.evaluate(function() {
         return document.body.offsetHeight;
     })
-    console.log("getting height:", documentHeight)
+    //console.log("getting height:", documentHeight)
     return documentHeight;
 }
 
 var renderPage = function(page){
 
-    var url =  getPageURL(page);
     var pageHeight = getPageHeight(page);
 
     page.clipRect = {
-        top:0,left:0,width: SCREENSHOT_WIDTH,
-        height: SCREENSHOT_HEIGHT
+        top:0,left:0,width: SCREENSHOT_WIDTH,height: SCREENSHOT_HEIGHT
     };
-    page.render("public/images/" + page.title +".png");
-    console.log("rendered:", url+".png")
+    page.render("public/images/" + domains[index] +".png");
+    console.log("Rendered:", domains[index]+".png");
 }
 
 var exitIfLast = function(index,array){
-    console.log(array.length - index-1, "more screenshots to go!")
-    console.log("~~~~~~~~~~~~~~")
+    console.log(array.length - index-1, "more screenshots to go!");
+
     if (index == array.length-1){
-        console.log("exiting phantomjs")
+        console.log("Process finished, exiting PhantomJS");
         phantom.exit();
     }
 }
 
 var takeScreenshot = function(element){
 
-    console.log("opening URL:", element)
+    console.log("Opening URL:", element);
 
     var page = require("webpage").create();
 
     page.viewportSize = {width:SCREENSHOT_WIDTH, height:SCREENSHOT_HEIGHT};
-
     page.open(element);
 
-    console.log("waiting for page to load...")
+    console.log("Waiting for page to load...")
 
     page.onLoadFinished = function() {
-        setTimeout(function(){
-            console.log("that's long enough")
-            renderPage(page)
-            exitIfLast(index,URLS)
-            index++;
+			setTimeout(function() {
+
+            renderPage(page);
+            exitIfLast(index,URLS);
+						index++;
             takeScreenshot(URLS[index]);
+
         },LOAD_WAIT_TIME)
     }
+
 }
 
-var index = 0;
-
+//run the primary function
 takeScreenshot(URLS[index]);
